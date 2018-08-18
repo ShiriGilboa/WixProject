0aab5a0fc461ae87fad14649d3b466affdc884c9
ShiriGilboa <42292248+ShiriGilboa@users.noreply.github.com>
2018-08-18 17:58:48 +0300

    Update Gallery.js

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -56,10 +56,33 @@ class Gallery extends React.Component {
     const el = e.target.documentElement;
     const bottom = el.scrollHeight - el.scrollTop === el.clientHeight;
     if (bottom) {
-      this.getImages(this.props.tag);
-     this.setState({isLoading: false})
+      this.addMoreImages(this.props.tag);
      }
   }
+
+  addMoreImages(tag){
+
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&safe_search=1&nojsoncallback=1`;
+    const baseUrl = 'https://api.flickr.com/';
+    axios({
+      url: getImagesUrl,
+      baseURL: baseUrl,
+      method: 'GET'
+    })
+      .then(res => res.data)
+      .then(res => {
+        if (
+          res &&
+          res.photos &&
+          res.photos.photo &&
+          res.photos.photo.length > 0
+        ) {
+          this.setState((prevState)=>{
+            prevState.images.push(res.photos.photo);
+     });
+    }
+  });
+}
 
     clone(img){
       this.setState(
